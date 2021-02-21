9dc31f4fa7143903c048b8c0704f1e0f213d2cbd
skuyngoding <datapr68@gmail.com>
2021-02-21 23:27:59 +0700

    update ckeditor v5

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,14 +1,22 @@
 <div class="container-fluid">
 
-    <h3 class="h3 text-gray-800 my-2"><?= $judul ?></h3>
+    <h3 class="h3 text-gray-900 my-2"><?= $judul ?></h3>
 
     <div class="row">
         <div class="col-sm-8">
 
-            <form>
+            <!-- <form>
                 <h2>CKEditor</h2>
                 <textarea id="ckeditor" name="isi"></textarea>
-            </form>
+            </form> -->
+
+            <!-- The toolbar will be rendered in this container. -->
+            <div id="toolbar-container"></div>
+
+            <!-- This container will become the editable. -->
+            <div id="editor">
+                <p>Paragraph</p>
+            </div>
 
         </div>
     </div>
